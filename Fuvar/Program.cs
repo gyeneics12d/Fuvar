e39e6a856e70153b222d748547ead7fe2c95ce62
gyeneics12d <gyeneics.12d@acsjszki.hu>
2022-01-31 10:04:54 +0100

    Add project files.

---
+++ Fuvar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class Fuvar
+    {
+        public int taxi_id { get; set; }
+        public DateTime indulas { get; set; }
+        public int idotartam { get; set; }
+        public int tavolsag { get; set; }
+        public int viteldij { get; set; }
+        public int borravalo { get; set; }
+        public string fizetesmod { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
